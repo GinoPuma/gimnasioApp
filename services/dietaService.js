f1032a8650e5225339a81a59177ff01a5c12f27c
GinoPuma <ginopuma8@gmail.com>
2025-04-27 22:30:55 -0500

    a

--- services/dietaService.js
+++ services/dietaService.js
@@ -1,23 +1,25 @@
-const Dieta = require('../models/Dieta');
+const Dieta = require('../models/dieta');
 
-const crearDieta = async (data) => {
-    return await Dieta.create(data);
-};
+class DietaService {
+    async crearDieta(data) {
+        return await Dieta.create(data);
+    }
 
-const asignarDieta = async (dietaId, clienteId) => {
-    return await Dieta.findByIdAndUpdate(dietaId, { clienteId }, { new: true });
-};
+    async asignarDieta(dietaId, clienteId) {
+        return await Dieta.findByIdAndUpdate(dietaId, { clienteId }, { new: true });
+    }
 
-const obtenerDieta = async (dietaId) => {
-    return await Dieta.findById(dietaId).populate('clienteId').populate('entrenadorId');
-};
+    async obtenerDieta(dietaId) {
+        return await Dieta.findById(dietaId).populate('clienteId').populate('entrenadorId');
+    }
 
-const listarDietas = async () => {
-    return await Dieta.find().populate('clienteId').populate('entrenadorId');
-};
+    async listarDietas() {
+        return await Dieta.find().populate('clienteId').populate('entrenadorId');
+    }
 
-const eliminarDieta = async (dietaId) => {
-    return await Dieta.findByIdAndDelete(dietaId);
-};
+    async eliminarDieta(dietaId) {
+        return await Dieta.findByIdAndDelete(dietaId);
+    }
+}
 
-module.exports = { crearDieta, asignarDieta, obtenerDieta, listarDietas, eliminarDieta };
+module.exports = new DietaService();
